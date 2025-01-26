9d60d92e9f26bb1207d72a4f5ee906e60f1eba28
Kamiel de Visser <kamieldevisser@gmail.com>
2025-01-26 22:49:13 +0100

    add morse

--- morse/Program.cs
+++ morse/Program.cs
@@ -1,10 +1,40 @@
-﻿class Program {
-	public static void Main(string[] args)
-	{
-		int numArguments = args.Length;
-      Console.WriteLine(numArguments);
-		foreach (string arg in args) {
-			Console.WriteLine(arg);
-		}
-	}
+﻿class Program
+{
+   // string[] args is een array aan argumenten die meegegeven worden
+   // aan het programma.
+   public static void Main(string[] args)
+   {
+      string morse = args[0];
+
+      // We maken een map van stukjes morse naar de bijbehorende karakters
+      Dictionary<string, char> morseToCharMap = new Dictionary<string, char>
+      {
+         {"-", 'T'},
+         {"--", 'M'},
+         {".-", 'A'},
+         {"-.", 'N'},
+         {"---", 'O'},
+         {".", 'E'}
+      };
+
+      // We splitsen de morse string op in woorden
+      string[] words = morse.Split('/');
+
+      // Voor elk woord...
+      foreach (string word in words)
+      {
+         // ... splitsen we het woord op in de stukjes morse
+         string[] parts = word.Split(' ');
+         // Voor elk stukje morse...
+         foreach (string part in parts)
+         {
+            // ... printen we het bijbehorende karakter
+            Console.Write(morseToCharMap[part]);
+         }
+         // Na elk woord printen we een spatie
+         Console.Write(" ");
+      }
+      // We eindigen met een nieuwe regel
+      Console.WriteLine();
+   }
 }
