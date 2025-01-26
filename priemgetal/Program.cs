9cd91819c3ee3a6b2588609eea097e4f509c0563
Kamiel de Visser <kamieldevisser@gmail.com>
2025-01-26 23:29:19 +0100

    add priemgetal

---
+++ priemgetal/Program.cs
@@ -0,0 +1,41 @@
+﻿class Program
+{
+   static void Main(string[] args)
+   {
+      // We zetten eerst de input om naar een integer
+      // Zonder try-catch, maar nu met TryParse
+      if (int.TryParse(args[0], out int number))
+      {
+         // Als het een priemgetal is, schrijven we dat naar de console
+         if (IsPrime(number))
+         {
+            Console.WriteLine("Ja, dit is een priemgetal");
+         }
+         else
+         {
+            Console.WriteLine("Nee, dit is geen priemgetal");
+         }
+      }
+   }
+
+   // Functie om te controleren of een getal een priemgetal is
+   static bool IsPrime(int number)
+   {
+      // NB: negatieve getallen zijn per definitie geen priemgetallen
+      if (number <= 1)
+      {
+         return false;
+      }
+
+      // We controleren of het getal deelbaar is door een getal tussen 2 en de wortel van het getal
+      for (int i = 2; i <= Math.Sqrt(number); i++)
+      {
+         if (number % i == 0)
+         {
+            return false;
+         }
+      }
+
+      return true;
+   }
+}
