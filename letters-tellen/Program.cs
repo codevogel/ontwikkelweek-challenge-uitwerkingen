3391a885da419b2dae49ad99240562c7af9dd915
Kamiel de Visser <kamieldevisser@gmail.com>
2025-01-26 20:41:48 +0100

    add comments
    add comments

--- letters-tellen/Program.cs
+++ letters-tellen/Program.cs
@@ -1,31 +1,43 @@
-﻿class Program
+﻿// Hoofd class voor het programma.
+class Program
 {
    // string[] args is een array aan argumenten die meegegeven worden
    // aan het programma.
+   // Main is het startpunt van het programma.
+   // Wanneer het programma wordt gestart middels de command line,
+   // dan worden de argumenten meegegeven aan het programma.
+   // Bijvoorbeeld: dotnet run "Dit is een zin"
+   // Voor meer informatie, zie: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/main-and-command-args/
    public static void Main(string[] args)
    {
+      // Als er een verkeerd aantal argumenten is meegegeven, geef
+      // een foutmelding en stop het programma.
       if (args.Length != 1)
       {
          Console.WriteLine("error: Voer hoogstens 1 zin in");
          return;
       }
 
+      // Maak twee variabelen aan die het aantal letters en hoofdletters
+      // bijhouden.
       int countLetters = 0;
       int countCapital = 0;
+
+      // args[0] is de eerste string die is meegegeven aan het programma.
       string sentence = args[0];
 
+      // Loop door elke letter in de string en kijk...
       foreach (char c in args[0])
       {
+         // ... of het een letter is, en zo ja, verhoog countLetters.
          if (char.IsAsciiLetter(c))
-         {
             countLetters++;
-         }
+         // ... of het een hoofdletter is, en zo ja, verhoog countCapital.
          if (char.IsUpper(c))
-         {
             countCapital++;
-         }
       }
 
+      // Print het aantal letters en hoofdletters.
       Console.WriteLine("Aantal letters: " + countLetters);
       Console.WriteLine("Aantal hoofdletters: " + countCapital);
    }
--- optelsommen/Program.cs
+++ optelsommen/Program.cs
@@ -1,24 +1,35 @@
 ﻿class Program
 {
-		// string[] args is een array aan argumenten die meegegeven worden
-		// aan het programma.
-		public static void Main(string[] args)
-		{
-				if (args.Length != 2)
-				{
-						Console.WriteLine("error: voer twee getallen in");
-				}
-				try
-				{
-						int a = Int32.Parse(args[0]);
-						int b = Int32.Parse(args[1]);
-						int result = a + b;
-						Console.WriteLine(result);
-				}
-				catch (FormatException e)
-				{
-						Console.WriteLine("error: voer twee getallen in");
-				}
+   // string[] args is een array aan argumenten die meegegeven worden
+   // aan het programma.
+   public static void Main(string[] args)
+   {
+      // Als er geen twee argumenten zijn meegegeven, geef een error.
+      if (args.Length != 2)
+      {
+         Console.WriteLine("error: voer twee getallen in");
+         return;
+      }
 
-		}
+      // Dit is een try-catch block. Hiermee kan je errors opvangen.
+      // Een FormatException vindt plaats als de string niet omgezet kan worden
+      // naar een integer.
+      // Als deze exception optreedt in het try block, 
+      // wordt de code in het catch block uitgevoerd.
+      // Voor meer informatie over try-catch, zie:
+      // https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/keywords/try-catch
+      try
+      {
+         string numStringA = args[0];
+         string numStringB = args[1];
+         int numA = Int32.Parse(args[0]);
+         int numB = Int32.Parse(args[1]);
+         int result = numA + numB;
+         Console.WriteLine(result);
+      }
+      catch (FormatException)
+      {
+         Console.WriteLine("error: voer twee getallen in");
+      }
+   }
 }
