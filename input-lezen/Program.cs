4d9b65e5a8673f8841b5e3934ea33f6969c4d94d
Kamiel de Visser <kamieldevisser@gmail.com>
2025-01-26 21:28:11 +0100

    add input-lezen

---
+++ input-lezen/Program.cs
@@ -0,0 +1,33 @@
+﻿class Program
+{
+   // string[] args is een array aan argumenten die meegegeven worden
+   // aan het programma.
+   public static void Main(string[] args)
+   {
+      // Als er geen argumenten zijn meegegeven, print een foutmelding.
+      if (args.Length != 1)
+      {
+         Console.WriteLine("error: geef een bestand mee!");
+         return;
+      }
+
+      // Lees het eerste argument (het bestandspad) en zet deze in een string. 
+      string path = args[0];
+
+      // Check of het bestand bestaat.
+      if (!File.Exists(path))
+      {
+         Console.WriteLine("error: bestand bestaat niet!");
+         return;
+      }
+
+      // Lees de inhoud van het bestand en zet deze in een array van strings.
+      string[] lines = File.ReadAllLines(path);
+
+      // Loop door de array en print elke regel naar de console.
+      foreach (string line in lines)
+      {
+         Console.WriteLine(line);
+      }
+   }
+}
